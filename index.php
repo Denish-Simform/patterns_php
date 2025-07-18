2390d73d3353d196c43cf4930e3e53f62b17b7a4
Denish-Simform <denish@simformsolutions.com>
2025-07-18 17:37:29 +0530

    other patterns added

--- index.php
+++ index.php
@@ -1,76 +1,122 @@
 <?php
 echo "<pre>";
-    echo html_entity_decode("<h1>Pattern 1</h1>");
-    for($i=1;$i<6;$i++) {
-        for($j=(6-$i);$j<6;$j++) {
+
+// Pattern 1
+echo html_entity_decode("<h1>Pattern 1</h1>");
+for($i=1;$i<6;$i++) {
+    for($j=(6-$i);$j<6;$j++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+
+// Pattern 2
+echo str_repeat("<br>", 5);
+echo html_entity_decode("<h1>Pattern 2</h1>");
+for($i=1;$i<6;$i++) {
+    for($j=6-(6-$i);$j<6;$j++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+
+// Pattern 3
+echo str_repeat("<br>", 5);
+echo html_entity_decode("<h1>Pattern 3</h1>");
+for($i=1;$i<6;$i++) {
+    for($j=1;$j<6;$j++) {
+        if($j >= (6-$i)) {
             echo "*";
-        }
-        echo "<br>";
-    }
-
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-
-    echo html_entity_decode("<h1>Pattern 2</h1>");
-    for($i=1;$i<6;$i++) {
-        for($j=6-(6-$i);$j<6;$j++) {
-            echo "*";
-        }
-        echo "<br>";
-    }
-
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-
-    echo html_entity_decode("<h1>Pattern 3</h1>");
-    for($i=1;$i<6;$i++) {
-        for($j=1;$j<6;$j++) {
-            if($j >= (6-$i)) {
-                echo "*";
-            } else {
-                echo " ";
-            }
-        }
-        echo "<br>";
-    }
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
-
-    echo html_entity_decode("<h1>Pattern 4</h1>");
-    for($i=1;$i<4;$i++) {
-
-        for($j=1;$j<$i;$j++) {
+        } else {
             echo " ";
         }
-
-        for($k=6-$i-($i-1);$k>0;$k--) {
-            echo "*";         
-        }
-        echo "<br>";
-    }
-    echo " ";
-    for($i=3;$i<6;$i+=2) {
-
-        for($k=0;$k<$i;$k++) {
-            echo "*";         
-        }
-        echo "<br>";
     }
     echo "<br>";
+}
+
+// Pattern 4
+echo str_repeat("<br>", 5);
+echo html_entity_decode("<h1>Pattern 4</h1>");
+for($i=1;$i<4;$i++) {
+    for($j=1;$j<$i;$j++) {
+        echo " ";
+    }
+    for($k=6-$i-($i-1);$k>0;$k--) {
+        echo "*";         
+    }
     echo "<br>";
+}
+echo " ";
+for($i=3;$i<6;$i+=2) {
+    for($k=0;$k<$i;$k++) {
+        echo "*";         
+    }
     echo "<br>";
+}
+
+// Pattern 5: Inverted Right-Angle Triangle (Right-Aligned)
+echo str_repeat("<br>", 5);
+echo html_entity_decode("<h1>Pattern 5</h1>");
+for($i=5;$i>=1;$i--) {
+    for($j=1;$j<=5-$i;$j++) {
+        echo " ";
+    }
+    for($k=1;$k<=$i;$k++) {
+        echo "*";
+    }
     echo "<br>";
+}
+
+// Pattern 6: Pyramid
+echo str_repeat("<br>", 5);
+echo html_entity_decode("<h1>Pattern 6</h1>");
+for($i=1;$i<=5;$i++) {
+    for($j=1;$j<=5-$i;$j++) {
+        echo " ";
+    }
+    for($k=1;$k<=(2*$i-1);$k++) {
+        echo "*";
+    }
     echo "<br>";
+}
+
+// Pattern 7: Diamond
+echo str_repeat("<br>", 5);
+echo html_entity_decode("<h1>Pattern 7</h1>");
+$n = 5;
+for($i=1;$i<=$n;$i++) {
+    for($j=1;$j<=$n-$i;$j++) {
+        echo " ";
+    }
+    for($k=1;$k<=(2*$i-1);$k++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+for($i=$n-1;$i>=1;$i--) {
+    for($j=1;$j<=$n-$i;$j++) {
+        echo " ";
+    }
+    for($k=1;$k<=(2*$i-1);$k++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+
+// Pattern 8: Hollow Square
+echo str_repeat("<br>", 5);
+echo html_entity_decode("<h1>Pattern 8</h1>");
+$size = 5;
+for($i=1;$i<=$size;$i++) {
+    for($j=1;$j<=$size;$j++) {
+        if($i==1 || $i==$size || $j==1 || $j==$size) {
+            echo "*";
+        } else {
+            echo " ";
+        }
+    }
+    echo "<br>";
+}
 
 echo "</pre>";
-
 ?>
